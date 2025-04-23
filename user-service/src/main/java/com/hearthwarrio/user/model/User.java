8c183a635f4ab1015722675c0546c7577ff020cd
Ilya Kulivatsky <kulivatsky.ilya@gmail.com>
2025-04-23 17:52:35 +0300

    User Service + Dockerfiles

---
+++ user-service/src/main/java/com/hearthwarrio/user/model/User.java
@@ -0,0 +1,54 @@
+package com.hearthwarrio.user.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String email;
+    private String secret;
+
+    public User() {
+    }
+
+    public User(String username, String email, String secret) {
+        this.username = username;
+        this.email = email;
+        this.secret = secret;
+    }
+
+    // Getters & setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSecret() {
+        return secret;
+    }
+
+    public void setSecret(String secret) {
+        this.secret = secret;
+    }
+}
